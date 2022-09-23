b1c4eb404263182bbd978b7c01851bccab7ff9f3
旅途中看海 <554605951@qq.com>
2022-09-23 17:52:07 +0800

    '更新'

--- index.js
+++ index.js
@@ -1,18 +1,33 @@
 const fs = require("fs");
 
 
-const tmp = fs.readFileSync("./indexTpl.html",'utf8')
+let tmp = fs.readFileSync("./indexTpl.html",'utf8')
 
 const noCache = true;
+const timelog = true
 const publicUrl = "//sztoosimple.gitee.io/static-map/";
 // const scripts = tmp.match(/<script src[\w\W]+?\/script>/g)
-const scripts = ['/lib/leaflet@1.6.0.js',
-'/proj4@2.4.3.js',
-'/proj4leaflet1.0.1.min.js',
-"/plugins/leaflet.ChineseTmsProviders.js",
-'/lib/init.js']
-console.log(scripts)
-if(noCache){
+const scripts = ['lib/leaflet@1.6.0.js',
+'lib/proj4@2.4.3.js',
+'lib/proj4leaflet1.0.1.min.js',
+"plugins/leaflet.ChineseTmsProviders.js",
+'lib/init.js']
 
+const insertPostionStr ="<!-- <script>在这里插入script</script> -->"
+
+for(var i = 0;i<scripts.length;i++){
+    let script = publicUrl + scripts[i];
+    if(noCache){
+        script += `?r=${Date.now()}`
+    }
+    
+    scripts[i] = `<script src="${script}"></script>` 
+    if(timelog){
+        scripts[i] += `<script>console.log("${script.split("/").pop().split("?")[0]}",Date.now()-window.scriptLoadTime);window.scriptLoadTime=Date.now()</script>`
+    }
 }
-// console.log(tmp)
+
+
+tmp = tmp.replace(insertPostionStr,scripts.join("\n"));
+
+fs.writeFileSync("./index.html",tmp)
