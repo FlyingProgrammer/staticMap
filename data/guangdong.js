ad19ba12f8de13f30bc319bc8fc22515f3e1aec6
tuzhonghai <554605951@qq.com>
2022-11-12 21:43:26 +0800

    add guangdong

---
+++ data/guangdong.js
@@ -0,0 +1,213 @@
+window.markers = 
+	[
+		{
+			"fname": "石坑崆",
+			"falt": 1902,
+			"flng": 112.9908,
+			"flat": 24.9275,
+			"fcounty": "清远市阳山县,郴州市宜章县,韶关市乳源瑶族自治县",
+			"fcity": "清远市",
+			"fprom": 1592,
+			"findex": 139
+		},
+		{
+			"fname": "石坑崆",
+			"falt": 1902,
+			"flng": 112.9908,
+			"flat": 24.9275,
+			"fcounty": "清远市阳山县,郴州市宜章县,韶关市乳源瑶族自治县",
+			"fcity": "韶关市",
+			"fprom": 1592,
+			"findex": 139
+		},
+		{
+			"fname": "大田顶",
+			"falt": 1704,
+			"flng": 111.215,
+			"flat": 22.2908,
+			"fcounty": "茂名市信宜市",
+			"fcity": "茂名市",
+			"fprom": 1590,
+			"findex": 140
+		},
+		{
+			"fname": "大稠顶",
+			"falt": 1626,
+			"flng": 112.4158,
+			"flat": 24.2883,
+			"fcounty": "清远市阳山县,肇庆市怀集县",
+			"fcity": "肇庆市",
+			"fprom": 578,
+			"findex": 4748
+		},
+		{
+			"fname": "铜鼓嶂",
+			"falt": 1559,
+			"flng": 116.3508,
+			"flat": 24.175,
+			"fcounty": "梅州市丰顺县",
+			"fcity": "梅州市",
+			"fprom": 1373,
+			"findex": 259
+		},
+		{
+			"fname": "凤鸟髻",
+			"falt": 1498,
+			"flng": 116.63,
+			"flat": 23.9733,
+			"fcounty": "梅州市丰顺县,潮州市潮安区",
+			"fcity": "潮州市",
+			"fprom": 708,
+			"findex": 2622
+		},
+		{
+			"fname": "黄牛石",
+			"falt": 1430,
+			"flng": 114.4558,
+			"flat": 24.5092,
+			"fcounty": "河源市连平县,赣州市龙南市",
+			"fcity": "河源市",
+			"fprom": 968,
+			"findex": 934
+		},
+		{
+			"fname": "鹅凰嶂",
+			"falt": 1337,
+			"flng": 111.4142,
+			"flat": 21.8433,
+			"fcounty": "茂名市电白区,阳江市阳春市,阳江市阳西县",
+			"fcity": "阳江市",
+			"fprom": 1181,
+			"findex": 454
+		},
+		{
+			"fname": "惠东莲花山",
+			"falt": 1336,
+			"flng": 115.2317,
+			"flat": 23.0608,
+			"fcounty": "惠州市惠东县,汕尾市海丰县",
+			"fcity": "惠州市",
+			"fprom": 1080,
+			"findex": 627
+		},
+		{
+			"fname": "惠东莲花山",
+			"falt": 1336,
+			"flng": 115.2317,
+			"flat": 23.0608,
+			"fcounty": "惠州市惠东县,汕尾市海丰县",
+			"fcity": "汕尾市",
+			"fprom": 1080,
+			"findex": 627
+		},
+		{
+			"fname": "龙须顶",
+			"falt": 1327,
+			"flng": 111.2683,
+			"flat": 22.4975,
+			"fcounty": "云浮市罗定市,茂名市信宜市",
+			"fcity": "云浮市",
+			"fprom": 700,
+			"findex": 2716
+		},
+		{
+			"fname": "天露山",
+			"falt": 1251,
+			"flng": 112.2492,
+			"flat": 22.4658,
+			"fcounty": "云浮市新兴县,江门市开平市",
+			"fcity": "江门市",
+			"fprom": 1156,
+			"findex": 494
+		},
+		{
+			"fname": "李望嶂",
+			"falt": 1222,
+			"flng": 115.8117,
+			"flat": 23.6417,
+			"fcounty": "揭阳市揭西县,梅州市五华县",
+			"fcity": "揭阳市",
+			"fprom": 491,
+			"findex": 7556
+		},
+		{
+			"fname": "天堂顶",
+			"falt": 1210,
+			"flng": 113.8146622,
+			"flat": 23.6546975,
+			"fcounty": "广州市从化区,惠州市龙门县",
+			"fcity": "广州市",
+			"fprom": null,
+			"findex": 300879
+		},
+		{
+			"fname": "梧桐山",
+			"falt": 944,
+			"flng": 114.215,
+			"flat": 22.5817,
+			"fcounty": "深圳市盐田区,深圳市罗湖区,深圳市龙岗区",
+			"fcity": "深圳市",
+			"fprom": 868,
+			"findex": 1322
+		},
+		{
+			"fname": "银瓶咀",
+			"falt": 898,
+			"flng": 114.2375,
+			"flat": 22.885,
+			"fcounty": "惠州市惠阳区",
+			"fcity": "东莞市",
+			"fprom": 257,
+			"findex": 38002
+		},
+		{
+			"fname": "鹤山",
+			"falt": 775,
+			"flng": 112.7767,
+			"flat": 22.7208,
+			"fcounty": "佛山市高明区,江门市鹤山市",
+			"fcity": "佛山市",
+			"fprom": 296,
+			"findex": 27246
+		},
+		{
+			"fname": "大尖山",
+			"falt": 587,
+			"flng": 116.9692,
+			"flat": 23.4325,
+			"fcounty": "汕头市南澳县",
+			"fcity": "汕头市",
+			"fprom": 564,
+			"findex": 5098
+		},
+		{
+			"fname": "黄杨山",
+			"falt": 581,
+			"flng": 113.245,
+			"flat": 22.2417,
+			"fcounty": "珠海市斗门区",
+			"fcity": "珠海市",
+			"fprom": 565,
+			"findex": 5067
+		},
+		{
+			"fname": "五桂山",
+			"falt": 531,
+			"flng": 113.4575,
+			"flat": 22.425,
+			"fcounty": null,
+			"fcity": "中山市",
+			"fprom": 512,
+			"findex": 6725
+		},
+		{
+			"fname": "双峰嶂",
+			"falt": 382,
+			"flng": 110.0708,
+			"flat": 21.8133,
+			"fcounty": "湛江市廉江市",
+			"fcity": "湛江市",
+			"fprom": 289,
+			"findex": 28856
+		}
+	]
